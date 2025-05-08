381d1b5c6bea50986e1e237c46b64e06e22af72a
maurochayle <38973588@fi.unju.edu.ar>
2025-05-08 16:16:36 -0300

    Creación carpeta Ejercicios_TP_Adicional y creación de Array

---
+++ Ejercicios_TP_Adicional/arrays.js
@@ -0,0 +1,7 @@
+const productos = [
+    {descripcion: "Teclado", precio: 30000},
+    {descripcion: "Mouse", precio: 25000},
+    {descripcion: "Monitor", precio: 120000},
+    {descripcion: "Microfono", precio: 45000},
+    {descripcion: "Parlantes", precio: 20000}
+];
